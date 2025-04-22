30ef21628380d8a18e4b9f7377e9d2b77f25ed60
Guilherme Santos <68071170+guilhermehvsantos@users.noreply.github.com>
2025-04-22 20:55:39 -0300

    Update CorsConfig.java

--- src/main/java/com/senac/FixIt/config/CorsConfig.java
+++ src/main/java/com/senac/FixIt/config/CorsConfig.java
@@ -17,6 +17,8 @@ public class CorsConfig {
                         .allowedOriginPatterns(
                                 "http://localhost:3000", // dev local
                                 "https://fixit-pi-five.vercel.app", // produção (vercel)
+                                "https://fixit-pi.vercel.app",
+                                "fixit-pi-cinco.vercel.app",
                                 "https://v0-saa-s-landing-page-c4psf4.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
